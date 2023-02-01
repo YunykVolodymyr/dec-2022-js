70c75caa40f82e9d76a3fea1d049cd80fdc7ffc3
YunykVolodymyr <yunyk.v.v@gmail.com>
2023-02-01 02:06:43 +0200

    Added lesson1

---
+++ lesson1/main.js
@@ -0,0 +1,39 @@
+let hello, owu, com, ua, num1, num10, num_999, num123, num3_14, num2_7, num16, b_true, b_false;
+hello = 'hello';
+owu = 'owu';
+com = 'com';
+ua = 'ua';
+num1 = 1;
+num10 = 10;
+num_999 = -999;
+num123 = 123;
+num3_14 = 3.14;
+num2_7 = 2.7;
+num16 = 16;
+b_true = true;
+b_false = false;
+
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(num1);
+console.log(num10);
+console.log(num_999);
+console.log(num123);
+console.log(num3_14);
+console.log(num2_7);
+console.log(num16);
+console.log(b_true);
+console.log(b_false);
+
+let firstName = 'Volodymyr', middleName = 'Vasylovych', lastName = 'Yunyk';
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+let a = 100, b = '100', c = true;
+console.log(typeof a, typeof b, typeof  c);
+
+console.log(prompt('Enter your first name', firstName));
+console.log(prompt('Enter your middle name', middleName));
+console.log(prompt('Enter your age', '20'));
